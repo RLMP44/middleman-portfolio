a54eb9a09b83dee0fab1ceb620ab034b892671c8
Rachael Momber <rlmomber@gmail.com>
2025-06-12 23:28:08 -0400

    refactor: remove unused ruby bionic conversion methods

--- config.rb
+++ config.rb
@@ -31,57 +31,8 @@ page '/*.txt', layout: false
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-helpers do
-  def convert_to_bionic(words)
-    words_array = words.split(' ')
-    bionic_array = []
-    words_array.each do |word|
-      bionic_word = ''
-      if has_punctuation?(word) && has_hyphen?(word)
-        updated_word = word.chomp(word[-1])
-        bionic_word = handle_hyphen(updated_word, word)
-      elsif has_hyphen?(word)
-        bionic_word = handle_hyphen(nil, word)
-      elsif has_punctuation?(word)
-        updated_word = word.chomp(word[-1])
-        num_to_highlight = get_amount_to_highlight(updated_word)
-        bionic_word = highlight_word(word, num_to_highlight)
-      else
-        num_to_highlight = get_amount_to_highlight(word)
-        bionic_word = highlight_word(word, num_to_highlight)
-      end
-      bionic_array.push(bionic_word)
-    end
-    bionic_array.join(' ')
-  end
-
-  def has_hyphen?(word)
-    word.include?('-')
-  end
-
-  def handle_hyphen(no_punc_word, original_word)
-    word_array = no_punc_word ? no_punc_word.split('-') : original_word.split('-')
-    new_word = word_array.each.map do |word|
-      num_to_highlight = get_amount_to_highlight(word)
-      highlight_word(word, num_to_highlight)
-    end.join('-')
-    return no_punc_word ? (new_word + original_word[-1]) : new_word
-  end
-
-  def highlight_word(word, num_to_highlight)
-    first_half = word[0, num_to_highlight]
-    second_half = word[num_to_highlight..]
-    return "<strong>#{first_half}</strong>#{second_half}"
-  end
-
-  def has_punctuation?(word)
-    word[-1] =~ /\p{P}/ ? true : false
-  end
-
-  def get_amount_to_highlight(word)
-    (word.length.to_f / 2).ceil
-  end
-end
+# helpers do
+# end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
